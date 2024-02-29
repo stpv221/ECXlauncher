175b097477c7cf30c73cbf4ea0463427bb9c2e15
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-02-29 19:26:41 +0000

    WHHHHYYYYY

--- version.js
+++ version.js
@@ -1,8 +1,5 @@
-const win = window.open();
-
 const versionName = document.getElementsByClassName("version-name");
 
-const versionSelected = "latestRelease";
 
     for (let i = 0; i < versionName.length; i++){
         versionName.innerHTML= '<img src="/assets/icons/Enchanting.png" width="20px" height="auto"> Latest Release <span class="material-symbols-outlined">expand_more</span>'
