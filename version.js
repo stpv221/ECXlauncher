6d0058abf97da9e9be675abcf677682f24a0073c
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-02-29 19:47:01 +0000

    h

--- version.js
+++ version.js
@@ -1,3 +1,5 @@
-const versionName = document.querySelectorAll(".version-name");
+const versionName = document.querySelector(".version-name");
+const version = document.querySelector("version")
 
-versionName.innerHTML= '<img src="/assets/icons/Enchanting.png" width="20px" height="auto"> Latest Release <span class="material-symbols-outlined">expand_more</span>'
+versionName.innerHTML = '<img src="/assets/icons/Enchanting.png" width="20px" height="auto"> Latest Release <span class="material-symbols-outlined">expand_more</span>'
+version.innerHTML = 'u23'
