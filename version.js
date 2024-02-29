da0c4bd484d67f2a3c7fcec4d13acd0157084618
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-02-29 19:11:46 +0000

    working

--- version.js
+++ version.js
@@ -3,8 +3,6 @@ const versionName = document.getElementsByClassName("version-name");
 
 const versionSelected = "latestRelease";
 
-if (versionSelected == "latestRelease"){
     for (let i = 0; i < versionName.length; i++){
-        versionName.innerHTML= "<img src=\"/assets/icons/Enchanting.png\" width=\"20px\" height=\"auto\"> Latest Release <span class=\"material-symbols-outlined\">expand_more</span>"
+        versionName.innerHTML= '<img src="/assets/icons/Enchanting.png" width="20px" height="auto"> Latest Release <span class="material-symbols-outlined">expand_more</span>'
     }
-}
