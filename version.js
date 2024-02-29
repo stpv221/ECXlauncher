71a8800c3e684694b85ce6a39dac5949fcf287ae
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-02-29 19:58:05 +0000

    h

--- version.js
+++ version.js
@@ -1,5 +1,15 @@
+const versionSelected = "latest_release";
+
 const versionName = document.querySelector(".version-name");
 const version = document.querySelector(".version");
+const clientOpen = document.querySelector(".clientopen");
 
-versionName.innerHTML = '<img src="/assets/icons/Enchanting.png" width="20px" height="auto"> Latest Release <span class="material-symbols-outlined">expand_more</span>'
-version.innerHTML = 'u23'
+if (versionSelected == "latest_release"){
+    versionName.innerHTML = '<img src="/assets/icons/Enchanting.png" width="20px" height="auto"> Latest Release <span class="material-symbols-outlined">expand_more</span>'
+    version.innerHTML = 'u23'
+    clientOpen.onclick = 'launch_latest'
+}
+
+function launch_latest(){
+        
+}
