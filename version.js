507884e47fdd32dd7dae0d3366c310183a07d1e0
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-02-29 19:43:02 +0000

    h

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
-const versionName = document.querySelector(".version-name");
+const versionName = document.querySelectorAll(".version-name");
 
 versionName.innerHTML= '<img src="/assets/icons/Enchanting.png" width="20px" height="auto"> Latest Release <span class="material-symbols-outlined">expand_more</span>'
