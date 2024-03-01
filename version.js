aecd6b352b9cf853365f0aaf242133133027dc79
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-03-01 17:58:33 +0000

    j

--- version.js
+++ version.js
@@ -10,9 +10,9 @@ const clientOpen = document.querySelector(".clientopen");
 if (versionSelected == "latest_release"){
     versionName.innerHTML = '<img src="/assets/icons/Enchanting.png" width="20px" height="auto"> Latest Release <span class="material-symbols-outlined">expand_more</span>'
     version.innerHTML = 'u23'
-    clientOpen.onclick = function() {
+    /*clientOpen.onclick = function() {
         alert("button was clicked");
-     }​;​
+     }​;​*/
 }
 
 function ab(){
