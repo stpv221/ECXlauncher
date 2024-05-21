7038374dbcaba7bd829f2fa4e519384a8152eec8
stpv22 <167645243+stpv221@users.noreply.github.com>
2024-05-21 14:16:53 -0700

    test to see if I can convert the client to base64

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 const clientLaunch = document.getElementById("launch");
-clientLaunch.addEventListener("click", () =>{
-    alert("MEN");
-})
+
+const text = document.getElementById("testLol"); 
+
+async function getBase64ImageFromUrl(imageUrl) {
+    var res = await fetch(imageUrl);
+    var blob = await res.blob();
+  
+    return new Promise((resolve, reject) => {
+      var reader  = new FileReader();
+      reader.addEventListener("load", function () {
+          resolve(reader.result);
+      }, false);
+  
+      reader.onerror = () => {
+        return reject(this);
+      };
+      reader.readAsDataURL(blob);
+    })
+  }
+
+  getBase64ImageFromUrl('http://approvaltests.com/images/logo.png')
+    .then(result => text.innerText = result)
+    .catch(err => console.error(err));
